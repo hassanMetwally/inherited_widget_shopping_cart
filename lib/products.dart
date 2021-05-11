3007a5910252b6a3631701641c938d94ed288369
[hassanMetwally] <[has.metwally@gmail.com]>
2021-05-11 02:55:26 +0200

    replace statefulBuilder with statefulWidget [Products()]

--- lib/products.dart
+++ lib/products.dart
@@ -4,61 +4,138 @@ import 'package:flutter/material.dart';
 import 'package:inherited_widget_shopping_cart/shopping_cart.dart';
 import 'data_center.dart';
 
-class Products extends StatelessWidget {
+// class Products extends StatelessWidget {
+//   @override
+//    Widget build(BuildContext context) {
+//     List productsList =DataCenter.of(context).productsList;
+//     List shoppingCartItems = DataCenter.of(context).shoppingCartList;
+//     return StatefulBuilder(
+//         builder: (BuildContext context, StateSetter setState) {
+//       return Scaffold(
+//         appBar: AppBar(
+//           title: Text("Products"),
+//           actions: [
+//             IconButton(icon: Icon(Icons.refresh),
+//               onPressed: (){
+//                 setState((){});
+//               },),
+//
+//             Padding(
+//               padding: EdgeInsets.only(right: 20, top: 8),
+//               child: Badge(
+//                 badgeContent: Text('${shoppingCartItems.length}'),
+//
+//                 child: IconButton(icon: Icon(Icons.shopping_cart),
+//                     onPressed: (){
+//                       Navigator.of(context)
+//                           .push(MaterialPageRoute(builder: (context) {
+//                         return ShoppingCart();
+//                       }));
+//                     },)
+//
+//               ),
+//             )
+//           ],
+//         ),
+//         body: ListView.builder(
+//             itemCount: productsList.length,
+//             itemBuilder: (BuildContext context, position) {
+//               return Card(
+//                 margin: EdgeInsets.all(10),
+//                 child: ListTile(
+//                   title: Text("${productsList[position].name}"),
+//                   subtitle: Text("${productsList[position].price} EGP"),
+//                   trailing: Checkbox(
+//                     value: productsList[position].isChecked,
+//                     onChanged: (value){
+//                       _onPressed(setState, productsList, position, shoppingCartItems);
+//                     },
+//                   ),
+//                   onTap: (){
+//                     _onPressed(setState, productsList, position, shoppingCartItems);
+//                   },
+//                 ),
+//               );
+//             }),
+//       );
+//     });
+//   }
+//
+//   void _onPressed(StateSetter setState, List productsList, int position, List shoppingCartItems) {
+//     setState((){
+//       productsList[position].isChecked = !productsList[position].isChecked;
+//       if(productsList[position].isChecked){
+//         shoppingCartItems.add(productsList[position]);
+//       }
+//       else{
+//         shoppingCartItems.remove(productsList[position]);
+//       }
+//     });
+//   }
+//
+//
+// }
+
+class Products extends StatefulWidget {
+  const Products({Key key}) : super(key: key);
+
   @override
-   Widget build(BuildContext context) {
+  _ProductsState createState() => _ProductsState();
+}
+
+class _ProductsState extends State<Products> {
+  @override
+  Widget build(BuildContext context) {
     List productsList =DataCenter.of(context).productsList;
     List shoppingCartItems = DataCenter.of(context).shoppingCartList;
-    return StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Products"),
-          actions: [
-            IconButton(icon: Icon(Icons.refresh),
-              onPressed: (){
-                setState((){});
-              },),
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Products"),
+              actions: [
+                IconButton(icon: Icon(Icons.refresh),
+                  onPressed: (){
+                    setState((){});
+                  },),
 
-            Padding(
-              padding: EdgeInsets.only(right: 20, top: 8),
-              child: Badge(
-                badgeContent: Text('${shoppingCartItems.length}'),
+                Padding(
+                  padding: EdgeInsets.only(right: 20, top: 8),
+                  child: Badge(
+                      badgeContent: Text('${shoppingCartItems.length}'),
 
-                child: IconButton(icon: Icon(Icons.shopping_cart),
-                    onPressed: (){
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return ShoppingCart();
-                      }));
-                    },)
+                      child: IconButton(icon: Icon(Icons.shopping_cart),
+                        onPressed: (){
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return ShoppingCart();
+                          }));
+                        },)
 
-              ),
-            )
-          ],
-        ),
-        body: ListView.builder(
-            itemCount: productsList.length,
-            itemBuilder: (BuildContext context, position) {
-              return Card(
-                margin: EdgeInsets.all(10),
-                child: ListTile(
-                  title: Text("${productsList[position].name}"),
-                  subtitle: Text("${productsList[position].price} EGP"),
-                  trailing: Checkbox(
-                    value: productsList[position].isChecked,
-                    onChanged: (value){
-                      _onPressed(setState, productsList, position, shoppingCartItems);
-                    },
                   ),
-                  onTap: (){
-                    _onPressed(setState, productsList, position, shoppingCartItems);
-                  },
-                ),
-              );
-            }),
-      );
-    });
+                )
+              ],
+            ),
+            body: ListView.builder(
+                itemCount: productsList.length,
+                itemBuilder: (BuildContext context, position) {
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text("${productsList[position].name}"),
+                      subtitle: Text("${productsList[position].price} EGP"),
+                      trailing: Checkbox(
+                        value: productsList[position].isChecked,
+                        onChanged: (value){
+                          _onPressed(setState, productsList, position, shoppingCartItems);
+                        },
+                      ),
+                      onTap: (){
+                        _onPressed(setState, productsList, position, shoppingCartItems);
+                      },
+                    ),
+                  );
+                }),
+          );
+
   }
 
   void _onPressed(StateSetter setState, List productsList, int position, List shoppingCartItems) {
@@ -73,5 +150,5 @@ class Products extends StatelessWidget {
     });
   }
 
-
 }
+
